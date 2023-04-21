35ad32982d991dbb28929fb8b69a5f8d4d873c1f
Van Minh Do <112021890+SirVanMinhDo@users.noreply.github.com>
2023-04-21 09:07:35 -0400

    s11c2 debut de l'exercice 2

--- carrousel.php
+++ carrousel.php
@@ -17,36 +17,32 @@ function mon_enqueue_css_js()
     $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
 
     wp_enqueue_style(
-        'vmd_plugin_carrousel_css',
+        'em_plugin_carrousel_css',
         plugin_dir_url(__FILE__) . "style.css",
         array(),
         $version_css
     );
 
     wp_enqueue_script(
-        'vmd_plugin_carrousel_js',
+        'em_plugin_carrousel_js',
         plugin_dir_url(__FILE__) . "js/carrousel.js",
         array(),
         $version_js,
         true
     );
 }
+
 add_action('wp_enqueue_scripts', 'mon_enqueue_css_js');
+
 
 function creation_carrousel()
 {
-    return "<button class='carrousel__ouvrir> Ouvrir le carrousel </button>
-    <div class='carrousel'> 
+    return "<button class='carrousel__ouvrir'>Ouvrir le carrousel</button>
+    <div class='carrousel'>
     <button class='carrousel__x'>X</button>
     <figure class='carrousel__figure'></figure>
     <form class='carrousel__form'></form>
     </div> <!-- fin du carrousel -->
-    
     ";
 }
-add_shortcode('vmd_carrousel', 'creation_carrousel'); //hook
-
-// pour activer, worpress > tableau bord > article > Galerie acceuil >
-// ajout code dans éditeur de code par plugin gutherber > «<p>[carrousel]</p>»
-
-// on va ajouter un hook carrousel, et va executer la fonction carrousel en JS
+add_shortcode('carrousel', 'creation_carrousel');
