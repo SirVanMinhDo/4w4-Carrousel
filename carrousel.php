f5b8eef9b939637c8c5ea533437661f95509009d
Van Minh Do <112021890+SirVanMinhDo@users.noreply.github.com>
2023-05-01 14:23:40 -0400

    S13C1 Les fonctionalités du carrousel marchent, les images basculent et sont délimités. Les radio sont checked au fur et à mesure que les boutons avant et après sont clické

--- carrousel.php
+++ carrousel.php
@@ -37,9 +37,11 @@ add_action('wp_enqueue_scripts', 'mon_enqueue_css_js');
 
 function creation_carrousel()
 {
-    return "<button class='carrousel__ouvrir'>Ouvrir le carrousel</button>
+    return "
     <div class='carrousel'>
     <button class='carrousel__x'>X</button>
+    <button class='carrousel__precedant'><</button>
+    <button class='carrousel__suivant'>></button>
     <figure class='carrousel__figure'></figure>
     <form class='carrousel__form'></form>
     </div> <!-- fin du carrousel -->
--- js/carrousel.js
+++ js/carrousel.js
@@ -1,8 +1,10 @@
 (function () {
     console.log('Début du carrousel')
-    let carrousel__ouvrir = document.querySelector('.carrousel__ouvrir')
+    let carrousel__ouvrir = document.querySelector('.wp-block-gallery')
     let carrousel = document.querySelector('.carrousel')
     let carrousel__x = document.querySelector('.carrousel__x')
+    let carrousel__precedant = document.querySelector('.carrousel__precedant')
+    let carrousel__suivant = document.querySelector('.carrousel__suivant')
     let carrousel__figure = document.querySelector('.carrousel__figure')
     let carrousel__form = document.querySelector('.carrousel__form')
     console.log(carrousel__form.tagName) // conteneur de radio-boutons
@@ -10,27 +12,61 @@
     let galerie = document.querySelector('.galerie')
     let galerie__img = galerie.querySelectorAll('img')
 
-    carrousel__ouvrir.addEventListener('mousedown', function () {
-        carrousel.classList.add('carrousel--activer')
-        ajouter_les_images_de_galerie()
-    })
-
     carrousel__x.addEventListener('mousedown', function () {
         carrousel.classList.remove('carrousel--activer')
     })
+
+
+
+    carrousel__precedant.addEventListener('mousedown', function () {
+
+        if (index > 0) {
+            index--
+            console.log(index)
+        }
+
+        affiche_image_carrousel()
+
+    })
+
+    carrousel__suivant.addEventListener('mousedown', function () {
+
+        if (index < 6) {
+            index++
+        }
+        affiche_image_carrousel()
+
+    })
+
+
+
     /**
      * Pour chaque image de la galerie l'ajouter dans le carrousel
      */
     let position = 0
-    let index = 0
+    let index = [0, 7]
     let ancienIndex = -1
-    function ajouter_les_images_de_galerie() {
 
-        for (const elem of galerie__img) {
-            ajouter_une_image_dans_courrousel(elem)
-            ajouter_un_radio_bouton_dans_carrousel()
-        }
+
+
+
+    /** Boucle qui permet de construire le carrousel */
+    for (const elem of galerie__img) {
+        elem.dataset.index = position
+        elem.addEventListener('mousedown', function () {
+
+
+            carrousel.classList.add('carrousel--activer')
+
+
+
+            index = this.dataset.index
+            affiche_image_carrousel()
+        })
+        ajouter_une_image_dans_courrousel(elem)
+        ajouter_un_radio_bouton_dans_carrousel()
     }
+
 
     /**
      * Création dynamique d'une image pour le carrousel
@@ -50,14 +86,16 @@
         rad.setAttribute('name', 'carrousel__rad')
         rad.classList.add('carrousel__rad')
         rad.dataset.index = position
-        rad.addEventListener('mousedown', function () {
-            index = this.dataset.index
+        rad.addEventListener('mousedown', function (e) {
+            index = e.target.dataset.index
             affiche_image_carrousel()
 
 
         })
+
         position = position + 1 // incrémentation de la position
         carrousel__form.append(rad)
+
     }
 
     /**
@@ -68,11 +106,17 @@
         if (ancienIndex != -1) {
             carrousel__figure.children[ancienIndex].style.opacity = "0"
             // carrousel__figure.children[ancienIndex].classList.remove('carrousel__img--activer')
+
+            console.log(carrousel__form.children[ancienIndex])
+            carrousel__form.children[index].checked = true
+
         }
         //console.log(this.dataset.index)
         carrousel__figure.children[index].style.opacity = "1"
         // carrousel__figure.children[ancienIndex].classList.add('carrousel__img--activer')
+        carrousel__form.children[index].checked = true
         ancienIndex = index
+
     }
 
 
